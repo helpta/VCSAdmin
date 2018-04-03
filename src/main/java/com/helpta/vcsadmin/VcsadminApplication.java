158192b4a024fc14ccce085ad123ca88574ece58
王虹凯 <whk.private@gmail.com>
2018-04-03 17:55:35 +0800

    Add files via upload
    init

---
+++ src/main/java/com/helpta/vcsadmin/VcsadminApplication.java
@@ -0,0 +1,12 @@
+package com.helpta.vcsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcsadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VcsadminApplication.class, args);
+    }
+}
---
+++ src/main/java/com/helpta/vcsadmin/controller/TestRestController.java
@@ -0,0 +1,20 @@
+package com.helpta.vcsadmin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * For Test
+ *
+ * @author : HongKai.wang
+ * @date : 2018/4/3
+ */
+@RestController
+@RequestMapping("/test")
+public class TestRestController {
+
+    @RequestMapping("/hello")
+    public String hello(String name) {
+        return "hello " + name;
+    }
+}
