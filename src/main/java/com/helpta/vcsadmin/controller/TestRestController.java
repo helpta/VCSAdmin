235044b43e5b216a38b706a4e93c7d194379bc89
王虹凯 <whk.private@gmail.com>
2018-04-04 18:42:35 +0800

    尝试集成freemarker

--- src/main/java/com/helpta/vcsadmin/controller/TestRestController.java
+++ src/main/java/com/helpta/vcsadmin/controller/TestRestController.java
@@ -1,6 +1,10 @@
 package com.helpta.vcsadmin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,12 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @author : HongKai.wang
  * @date : 2018/4/3
  */
-@RestController
+@Controller
 @RequestMapping("/test")
 public class TestRestController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         return "hello there," + name;
+    }
+
+    @RequestMapping("/testFreemarker/{name}")
+    public String testFreemarker(@PathVariable("name") String name, ModelMap modelMap) {
+        modelMap.addAttribute("name", name);
+        return "testFreemarker";
     }
 }
